5c73a78e24e9b2ed3b95f66f5db56c33ebc64260
Firdavs Panoyev <firdavs@Firdavss-MacBook-Pro.local>
2024-10-15 00:03:38 +0500

    last

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import {Routes, Route, Outlet} from 'react-router-dom'
+import Home from "./pages/Home";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import RecipeDetail from './pages/RecipeDetail';
 
-const App = () => {
+function Layout(){
   return (
-    <div>App</div>
+    <>
+      <Navbar/>
+        <Outlet/>
+      <Footer/>
+    </>
   )
 }
+function App() {
+  return (
+    <div className='bg-black'>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='recipes/:id' element={<RecipeDetail />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
 
-export default App
+export default App;
